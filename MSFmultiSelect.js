9e15f7dbd1478848c5643e820c3be1c758c3bea7
balaJS <vvbala1995@gmail.com>
2020-11-20 22:01:40 +0530

    Theme 2 implementation in progress.

--- MSFmultiSelect.js
+++ MSFmultiSelect.js
@@ -6,29 +6,54 @@
 class MSFmultiSelect{
   constructor(select, settings={}) {
     this.select = select;
-    this.settings = settings;
     this.defultSettings={
+      theme: 'simple',
       width:350,
       height:30,
       appendTo:'body',
       className:''
-    }
+    };
+
+    settings['theme'] = settings['theme'] ? settings['theme'] : this.defultSettings['theme'];
+    this.settings = settings;
+
+    this.data = {};
     this.select.multiple=true;
     this.select.style.display='none';
     this.create();
+    this.log();
+    return this;
   }
   create(){
+    var self = this;
     var div=document.createElement('DIV');
     div.className='msf_multiselect_container';
     this.id='msf_multiselect_'+(document.querySelectorAll('.msf_multiselect_container').length+1);
     div.id=this.id;
-    var ul=document.createElement('UL'); 
+    var ul=document.createElement('UL');
     ul.className='msf_multiselect';
-    var textarea=document.createElement('textarea'); textarea.readOnly=true;
+    var textarea=document.createElement('textarea');
     textarea.style.height=(this.settings.height) ? this.settings.height+'px' : this.defultSettings.height+'px';
     textarea.style.width=(this.settings.width) ? this.settings.width+'px' : this.defultSettings.width+'px';
     textarea.className=(this.settings.className) ? this.settings.className : this.defultSettings.className;
-    var self=this,display="";
+    textarea.addEventListener('keyup', function(event) {
+      var searchVal = event.originalTarget.value.toLocaleLowerCase();
+      var options = document.querySelectorAll('.msf_multiselect label li');
+
+      self._showAllOptions();
+      if (searchVal.length < 1) return;
+
+      var optinVal;
+      options.forEach(function(option) {
+        optinVal = option.innerText.toLocaleLowerCase();
+        console.log(optinVal);
+        if (optinVal === '<select all>') return;
+        if (optinVal.indexOf(searchVal) !== 0) {
+          option.parentElement.classList.add('hidden');
+        }
+      });
+    });
+
     if(this.settings.selectAll){
       var label=document.createElement('label');
       var li=document.createElement('LI');
@@ -45,6 +70,10 @@ class MSFmultiSelect{
       label.appendChild(li);
       ul.appendChild(label);
     }
+    var selectedLabelsDiv = document.createElement('div');
+    selectedLabelsDiv.className = 'selectedLabelsDiv';
+    selectedLabelsDiv.style.width = textarea.style.width;
+
     for(var i=0,len=this.select.children.length; i<len; i++){
       var label=document.createElement('label');
       var li=document.createElement('LI');
@@ -52,6 +81,7 @@ class MSFmultiSelect{
       input.type='checkbox';
       input.disabled=(this.settings.readOnly) ? true: false;
       input.value=this.select.children[i].value;
+
       var caption=document.createTextNode(this.select.children[i].innerText);
       input.addEventListener('click',function(){
         if(this.checked){
@@ -68,17 +98,19 @@ class MSFmultiSelect{
       if(this.select.children[i].selected){
         li.className='active';
         input.checked=true;
-        display+=(display) ? ", "+this.select.children[i].innerText : this.select.children[i].innerText;
+        this.data[i] = 1;
       }else{
         li.className='';
         input.checked=false;
+        this.data[i] = 0;
       }
       label.appendChild(li);
       ul.appendChild(label);
     }
-    textarea.value=display;
+
     this.logger=textarea;
     div.appendChild(textarea);
+    div.appendChild(selectedLabelsDiv);
     var addTarget;
     if(this.settings.appendTo){
       addTarget=document.querySelector(this.settings.appendTo);
@@ -116,11 +148,17 @@ class MSFmultiSelect{
             var sync=(this.settings.selectAll) ? (i+1) : i;
             this.list.children[sync].children[0].children[0].checked=true;
             this.list.children[sync].children[0].className='active'; //need to change;
+            this.data[i] = 1;
             break;
           }
         }
       }
       this.log();
+
+      var textarea = document.querySelector('.msf_multiselect_container textarea');
+      textarea.value = '';
+      textarea.focus()
+      this._showAllOptions();
     }
   }
   removeValue(selected=[]){
@@ -132,6 +170,7 @@ class MSFmultiSelect{
             var sync=(this.settings.selectAll) ? (i+1) : i;
             this.list.children[sync].children[0].children[0].checked=false;
             this.list.children[sync].children[0].className=''; //need to change;
+            this.data[i] = 0;
             break;
           }
         }
@@ -139,14 +178,39 @@ class MSFmultiSelect{
       this.log();
     }
   }
-  log(){
-    var display="";
-    for(var i=0; i<this.select.children.length; i++){
-      if(this.select.children[i].selected){
-        display+=(display) ? ", "+this.select.children[i].innerText : this.select.children[i].innerText;
+  log() {
+    var self=this;
+    var selectedLabelsDiv = document.getElementsByClassName('selectedLabelsDiv')[0];
+    selectedLabelsDiv.innerHTML = '';
+
+    var i = 0, option = '', selectedOptions = '';
+    var loop_length = this.select.children.length;
+
+    for(i; i < loop_length; i++) {
+      option = this.select.children[i];
+      if (!this.data[i]) {
+        continue;
       }
+
+      var selectedLabels = document.createElement('label');
+      selectedLabels.className = 'selectedLabels';
+      selectedLabels.innerHTML = option.innerText;
+
+      var closeBtn = document.createElement('span');
+      closeBtn.className = 'closeBtn';
+      closeBtn.innerHTML = 'X';
+      closeBtn.dataset.id = option.value;
+      closeBtn.addEventListener('click', function(event) {
+        self.removeValue([event.originalTarget.dataset.id]);
+      });
+
+      selectedLabels.appendChild(closeBtn);
+      selectedLabelsDiv.appendChild(selectedLabels);
+
+      selectedOptions += selectedOptions ? ',' + option.innerText : option.innerText;
     }
-    this.logger.value=display;
+    // TODO: update selectedOptions in input or select box.
+    console.log(selectedOptions);
   }
   getData(){
     var data=[];
@@ -192,5 +256,11 @@ class MSFmultiSelect{
     this.container.remove();
     this.create();
   }
-}
+  _showAllOptions() {
+    var options = document.querySelectorAll('.msf_multiselect label li');
 
+    options.forEach(function(option) {
+      option.parentElement.classList.remove('hidden');
+    });
+  }
+}
