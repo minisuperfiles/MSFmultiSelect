616b3f5c5df8170e457e9fd286b0b8c3ef7d84bd
balaJS <vvbala1995@gmail.com>
2020-11-21 03:08:14 +0530

    Two themes are separated and some refactoring is partially done.

--- MSFmultiSelect.js
+++ MSFmultiSelect.js
@@ -4,39 +4,65 @@
  * https://minisuperfiles.blogspot.com
  */
 class MSFmultiSelect{
-  constructor(select, settings={}) {
+  constructor(select, settings = {}) {
     this.select = select;
-    this.defultSettings={
-      theme: 'simple',
-      width:350,
-      height:30,
-      appendTo:'body',
-      className:''
-    };
-
-    settings['theme'] = settings['theme'] ? settings['theme'] : this.defultSettings['theme'];
-    this.settings = settings;
-
-    this.data = {};
     this.select.multiple=true;
     this.select.style.display='none';
+
+    this.settings = this._getSettings(settings);
+
+    this.class = {
+      searchBox: 'searchbox'
+    };
+
+    this.data = {};
+
     this.create();
     this.log();
     return this;
   }
+  _getSettings(settings) {
+    var defultSettings = {
+      theme: 'advanced',
+      width: '350px',
+      height: '20px',
+      appendTo:'body',
+      className: ''
+    };
+
+    Object.keys(defultSettings).forEach(function(attr) {
+      if (attr && settings[attr] !== undefined) return;
+      settings[attr] = defultSettings[attr];
+    });
+
+    return settings;
+  }
   create(){
     var self = this;
+    var addTarget = document.querySelector(this.settings.appendTo);
     var div=document.createElement('DIV');
     div.className='msf_multiselect_container';
     this.id='msf_multiselect_'+(document.querySelectorAll('.msf_multiselect_container').length+1);
     div.id=this.id;
-    var ul=document.createElement('UL');
-    ul.className='msf_multiselect';
-    var textarea=document.createElement('textarea');
-    textarea.style.height=(this.settings.height) ? this.settings.height+'px' : this.defultSettings.height+'px';
-    textarea.style.width=(this.settings.width) ? this.settings.width+'px' : this.defultSettings.width+'px';
-    textarea.className=(this.settings.className) ? this.settings.className : this.defultSettings.className;
-    textarea.addEventListener('keyup', function(event) {
+
+    // Creating theme specific elements here.
+    this.settings['theme'] === 'simple' ?
+      this._getThemeOneSpecificElems(div) : this._getThemeTwoSpecificElems(div);
+
+    // Creating common elements for both themes here.
+    this._getCommonElems(div);
+
+    this.container=div;
+    addTarget.appendChild(div);
+    // add event
+    document.addEventListener('click', function(event) {
+      var theme2Specific = self.settings['theme'] === 'simple' ? false : event.target.className === 'closeBtn';
+      if (self.container.contains(event.target) || theme2Specific) return;
+      self.list.classList.add('hidden');
+      self.searchBox.classList.add('hidden');
+    });
+
+    this.searchBox.addEventListener('keyup', function(event) {
       var searchVal = event.originalTarget.value.toLocaleLowerCase();
       var options = document.querySelectorAll('.msf_multiselect label li');
 
@@ -46,97 +72,12 @@ class MSFmultiSelect{
       var optinVal;
       options.forEach(function(option) {
         optinVal = option.innerText.toLocaleLowerCase();
-        console.log(optinVal);
+
         if (optinVal === '<select all>') return;
         if (optinVal.indexOf(searchVal) !== 0) {
           option.parentElement.classList.add('hidden');
         }
       });
-    });
-
-    if(this.settings.selectAll){
-      var label=document.createElement('label');
-      var li=document.createElement('LI');
-      var input=document.createElement('input');
-      input.type='checkbox';
-      input.disabled=(this.settings.readOnly) ? true: false;
-      input.addEventListener('click',function(){
-        this.parentElement.className=(this.checked) ? 'active' : '';
-        self.selectAll(this.checked);
-      });
-      var caption=document.createTextNode('<Select all>');
-      li.appendChild(input);
-      li.appendChild(caption);
-      label.appendChild(li);
-      ul.appendChild(label);
-    }
-    var selectedLabelsDiv = document.createElement('div');
-    selectedLabelsDiv.className = 'selectedLabelsDiv';
-    selectedLabelsDiv.style.width = textarea.style.width;
-
-    for(var i=0,len=this.select.children.length; i<len; i++){
-      var label=document.createElement('label');
-      var li=document.createElement('LI');
-      var input=document.createElement('input');
-      input.type='checkbox';
-      input.disabled=(this.settings.readOnly) ? true: false;
-      input.value=this.select.children[i].value;
-
-      var caption=document.createTextNode(this.select.children[i].innerText);
-      input.addEventListener('click',function(){
-        if(this.checked){
-          self.setValue([this.value]);
-        }else{
-          self.removeValue([this.value]);
-        }
-        if(typeof self.settings.onChange=='function'){
-          self.settings.onChange(this.checked,this.value,self);
-        }
-      });
-      li.appendChild(input);
-      li.appendChild(caption);
-      if(this.select.children[i].selected){
-        li.className='active';
-        input.checked=true;
-        this.data[i] = 1;
-      }else{
-        li.className='';
-        input.checked=false;
-        this.data[i] = 0;
-      }
-      label.appendChild(li);
-      ul.appendChild(label);
-    }
-
-    this.logger=textarea;
-    div.appendChild(textarea);
-    div.appendChild(selectedLabelsDiv);
-    var addTarget;
-    if(this.settings.appendTo){
-      addTarget=document.querySelector(this.settings.appendTo);
-    }else{
-      addTarget=document.querySelector(this.defultSettings.appendTo);
-    }
-    //set width
-    ul.style.width=(this.settings.width) ? this.settings.width+2+'px' : this.defultSettings.width+2+'px';
-    ul.style.display='none';
-    div.appendChild(ul);
-    this.list=ul;
-    this.container=div;
-    addTarget.appendChild(div);
-    // add event
-    document.addEventListener('click',function(event){
-      var isClickInside=self.container.contains(event.target);
-      if(!isClickInside){
-        self.list.style.display='none';
-      }
-    });
-    this.logger.addEventListener('click',function(){
-      if(self.list.style.display=='none'){
-        self.list.style.display='';
-      }else{
-        self.list.style.display='none';
-      }
     });
   }
   setValue(selected=[]){
@@ -155,9 +96,8 @@ class MSFmultiSelect{
       }
       this.log();
 
-      var textarea = document.querySelector('.msf_multiselect_container textarea');
-      textarea.value = '';
-      textarea.focus()
+      this.searchBox.value = '';
+      this.searchBox.focus();
       this._showAllOptions();
     }
   }
@@ -179,38 +119,8 @@ class MSFmultiSelect{
     }
   }
   log() {
-    var self=this;
-    var selectedLabelsDiv = document.getElementsByClassName('selectedLabelsDiv')[0];
-    selectedLabelsDiv.innerHTML = '';
-
-    var i = 0, option = '', selectedOptions = '';
-    var loop_length = this.select.children.length;
-
-    for(i; i < loop_length; i++) {
-      option = this.select.children[i];
-      if (!this.data[i]) {
-        continue;
-      }
-
-      var selectedLabels = document.createElement('label');
-      selectedLabels.className = 'selectedLabels';
-      selectedLabels.innerHTML = option.innerText;
-
-      var closeBtn = document.createElement('span');
-      closeBtn.className = 'closeBtn';
-      closeBtn.innerHTML = 'X';
-      closeBtn.dataset.id = option.value;
-      closeBtn.addEventListener('click', function(event) {
-        self.removeValue([event.originalTarget.dataset.id]);
-      });
-
-      selectedLabels.appendChild(closeBtn);
-      selectedLabelsDiv.appendChild(selectedLabels);
-
-      selectedOptions += selectedOptions ? ',' + option.innerText : option.innerText;
-    }
-    // TODO: update selectedOptions in input or select box.
-    console.log(selectedOptions);
+    this.settings['theme'] === 'simple' ?
+      this._ThemeOneSpecific_log() : this._ThemeTwoSpecific_log();
   }
   getData(){
     var data=[];
@@ -257,10 +167,158 @@ class MSFmultiSelect{
     this.create();
   }
   _showAllOptions() {
+    if (this.list.classList.contains('hidden')) this.list.remove('hidden');
     var options = document.querySelectorAll('.msf_multiselect label li');
 
     options.forEach(function(option) {
       option.parentElement.classList.remove('hidden');
     });
+  }
+  _getCommonElems(wrapper) {
+    var self = this;
+
+    var searchBox = document.createElement('input');
+    searchBox.type = 'text';
+    searchBox.className = this.class['searchBox'];
+    // TODO: we should set px or other styles in constructor.
+    searchBox.style.width = this.settings.width;
+    searchBox.classList.add('hidden');
+
+    var ul = document.createElement('UL');
+    ul.className = 'msf_multiselect';
+    ul.style.width = this.settings.width;
+    ul.classList.add('hidden');
+
+    if(this.settings.selectAll){
+      var label=document.createElement('label');
+      var li=document.createElement('LI');
+      var input=document.createElement('input');
+      input.type='checkbox';
+      input.disabled=(this.settings.readOnly) ? true: false;
+      input.addEventListener('click',function(){
+        this.parentElement.className=(this.checked) ? 'active' : '';
+        self.selectAll(this.checked);
+      });
+      var caption=document.createTextNode('<Select all>');
+      li.appendChild(input);
+      li.appendChild(caption);
+      label.appendChild(li);
+      ul.appendChild(label);
+    }
+
+    for(var i=0,len=this.select.children.length; i<len; i++){
+      var label=document.createElement('label');
+      var li=document.createElement('LI');
+      var input=document.createElement('input');
+      input.type='checkbox';
+      input.disabled=(this.settings.readOnly) ? true: false;
+      input.value=this.select.children[i].value;
+
+      var caption=document.createTextNode(this.select.children[i].innerText);
+      input.addEventListener('click',function(){
+        if(this.checked){
+          self.setValue([this.value]);
+        }else{
+          self.removeValue([this.value]);
+        }
+        if(typeof self.settings.onChange=='function'){
+          self.settings.onChange(this.checked,this.value,self);
+        }
+      });
+      li.appendChild(input);
+      li.appendChild(caption);
+      if(this.select.children[i].selected){
+        li.className='active';
+        input.checked=true;
+        this.data[i] = 1;
+      }else{
+        li.className='';
+        input.checked=false;
+        this.data[i] = 0;
+      }
+      label.appendChild(li);
+      ul.appendChild(label);
+    }
+
+    wrapper.appendChild(searchBox);
+    wrapper.appendChild(ul);
+
+    this.list = ul;
+    this.searchBox = searchBox;
+  }
+  _getThemeOneSpecificElems(wrapper) {
+    var self = this;
+    var textarea=document.createElement('textarea');
+    textarea.style.height = this.settings.height;
+    textarea.style.width = this.settings.width;
+    textarea.className = this.settings.className;
+    textarea.readOnly = true;
+
+    this.logger=textarea;
+    wrapper.appendChild(textarea);
+
+    this.logger.addEventListener('click', function() {
+      self.list.classList.toggle('hidden');
+      self.searchBox.classList.toggle('hidden');
+      if (!self.searchBox.classList.contains('hidden')) self.searchBox.focus();
+    });
+  }
+  _getThemeTwoSpecificElems(wrapper) {
+    // TODO: this is temporary fix. we need to implement separate element for theme2.
+    this._getThemeOneSpecificElems(wrapper);
+
+    var selectedLabelsDiv = document.createElement('div');
+    selectedLabelsDiv.className = 'selectedLabelsDiv';
+    selectedLabelsDiv.style.width = this.settings.width;
+    wrapper.appendChild(selectedLabelsDiv);
+  }
+  _ThemeOneSpecific_log() {
+    var i = 0, option = '', selectedOptions = '';
+    var loop_length = this.select.children.length;
+
+    for(i; i < loop_length; i++) {
+      option = this.select.children[i];
+      if (!this.data[i]) {
+        continue;
+      }
+
+      selectedOptions += selectedOptions ? ',' + option.innerText : option.innerText;
+    }
+
+    this.logger.value = selectedOptions;
+  }
+  _ThemeTwoSpecific_log() {
+    var self=this;
+    var selectedLabelsDiv = document.getElementsByClassName('selectedLabelsDiv')[0];
+    selectedLabelsDiv.innerHTML = '';
+
+    var i = 0, option = '', selectedOptions = '';
+    var loop_length = this.select.children.length;
+
+    for(i; i < loop_length; i++) {
+      option = this.select.children[i];
+      if (!this.data[i]) {
+        continue;
+      }
+
+      var selectedLabels = document.createElement('label');
+      selectedLabels.className = 'selectedLabels';
+      selectedLabels.innerHTML = option.innerText;
+
+      var closeBtn = document.createElement('span');
+      closeBtn.className = 'closeBtn';
+      closeBtn.innerHTML = 'X';
+      closeBtn.dataset.id = option.value;
+      closeBtn.addEventListener('click', function(event) {
+        self.removeValue([event.originalTarget.dataset.id]);
+      });
+
+      selectedLabels.appendChild(closeBtn);
+      selectedLabelsDiv.appendChild(selectedLabels);
+
+      selectedOptions += selectedOptions ? ',' + option.innerText : option.innerText;
+    }
+    // TODO: update selectedOptions in input or select box.
+    console.log(selectedOptions);
   }
 }
