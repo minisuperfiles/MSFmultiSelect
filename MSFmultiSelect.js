8e751259e30ec2609ac882d59483f812131411a9
balaJS <vvbala1995@gmail.com>
2020-12-01 23:04:40 +0530

    now searched results only will selected when user clicks select all btn.

--- MSFmultiSelect.js
+++ MSFmultiSelect.js
@@ -90,13 +90,19 @@ class MSFmultiSelect {
     if (searchVal.length < 1) return;
 
     var i, optinVal, option, optionsLen = options.length;
+    var allSelected = true;
     for (i = 0; i < optionsLen; i++) {
       option = options[i];
       optinVal = option.innerText.toLocaleLowerCase();
 
       if (optinVal.indexOf(searchVal) !== 0) {
         option.parentElement.classList.add('hidden');
+        continue;
       }
+      allSelected &&= option.firstChild.checked;
+    }
+    if (!allSelected) {
+      self.list.querySelector('li.ignore input[type="checkbox"]').checked = allSelected;
     }
   }
   _handleSearchBox() {
@@ -165,7 +171,7 @@ class MSFmultiSelect {
     searchResult = this._getLi(this.list, 'label:not(.hidden) li:not(.ignore)');
     selectedSearchResult = this._getLi(this.list, 'label:not(.hidden) li.active:not(.ignore)');
 
-    if (searchResult.length === selectedSearchResult.length || !selectedSearchResult.length) {
+    if (!selectedSearchResult.length) {
       this._handleSearchBox();
       this._showAllOptions();
     }
@@ -187,10 +193,12 @@ class MSFmultiSelect {
   }
   selectAll(isSetValue = false) {
     var data = [];
-    var i, selectChildrenLen = this.select.children.length;
+    var i, selectedChildren, selectChildrenLen;
+    selectedChildren = this.list.querySelectorAll('label:not(.hidden) li:not(.ignore) input');
+    selectChildrenLen = selectedChildren.length;
 
     for (i = 0; i < selectChildrenLen; i++) {
-      data.push(this.select.children[i].value);
+      data.push(selectedChildren[i].value);
     }
 
     isSetValue ? this.setValue(data) : this.removeValue(data);
